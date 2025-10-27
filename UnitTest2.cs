03fcc4b756586940bab8029914fa9309a4aa432a
ProfBlanc <34591333+ProfBlanc@users.noreply.github.com>
2025-10-27 17:03:08 -0400

    Create UnitTest2.cs

---
+++ UnitTest2.cs
@@ -0,0 +1,25 @@
+using ConsoleApp11WorkingWithTests;
+
+namespace WorkingWithTestsTestProject2
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        /*
+         * 
+         *          create a method to initialize a Student object
+         *          create test methods that test
+         *              create student name process
+         *          create data test method
+         *              tests adding valid grades
+         *          create data test method
+         *              tests adding invalid grades
+         *                  expect an exception
+         *          create a tests method
+         *              tests the grade counter method
+         * 
+         * 
+         */ 
+
+    }
+}
